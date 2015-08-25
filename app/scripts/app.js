2b66b11f91920c92d39ccdc296026b9d4438f42b
Janoo Fernandes <janoo@simplymeasured.com>
2015-08-24 21:44:03 -0700

    Clean up styles, update JS for Menu page, clean up main.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,9 +23,9 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/menu', {
+        templateUrl: 'views/menu.html',
+        controller: 'MenuCtrl'
       })
       .otherwise({
         redirectTo: '/'
